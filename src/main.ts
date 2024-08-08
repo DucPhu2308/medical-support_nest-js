c58b543f2f5e1d68c9a8acd7d8cc3a2892b1d32f
TrongPhucNguyen <121226705+galaticwarrior123@users.noreply.github.com>
2024-08-08 17:57:08 +0700

    add file .env

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+  app.enableCors({
+    origin: 'http://localhost:3000', // Thay đổi URL này thành nguồn bạn muốn cho phép
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.APP_PORT || 3000);
+  
 }
 bootstrap();
