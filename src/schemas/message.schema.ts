bc51406f710ad31c0e992b5de0afe7e99999629e
Duc Phu <nguyenducphu2308@gmail.com>
2024-08-13 13:16:12 +0700

    update message schema

--- src/schemas/message.schema.ts
+++ src/schemas/message.schema.ts
@@ -1,15 +1,37 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { BaseSchema } from "./base.schema";
 import mongoose, { HydratedDocument, Types } from "mongoose";
-import { User } from "./user.schema";
+
+export enum MessageType {
+    TEXT = 'text',
+    IMAGE = 'image',
+    APPOINTMENT = 'appointment',
+    FILE = 'file',
+    CALL = 'call',
+}
+
+export interface AppointmentMessage {
+    title: string;
+    content: string;
+    date: Date;
+    isAccepted: boolean;
+}
+
+export interface CallMessage {
+    duration: number;
+    isAccepted: boolean;
+}
 
 @Schema({timestamps: true})
 export class Message extends BaseSchema {
     @Prop({ required: true })
-    content: string;
+    content: string | AppointmentMessage | CallMessage;
 
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
     sender: Types.ObjectId;
+
+    @Prop({ enum: MessageType})
+    type: MessageType;
 }
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
