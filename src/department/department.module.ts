af6855c242ecc3dcba31aecda67f8585b12e6b64
TrongPhucNguyen <trongnguyenphuc22082003@gmail.com>
2024-11-06 19:42:36 +0700

    update delete department

--- src/department/department.module.ts
+++ src/department/department.module.ts
@@ -3,10 +3,14 @@ import { SpectialityController } from './department.controller';
 import { SpectialityService } from './department.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Speciality, SpecialitySchema } from 'src/schemas/speciality.schema';
+import { Post, PostSchema } from 'src/schemas/post.schema';
+import { DoctorInfo, DoctorInfoSchema } from 'src/schemas/doctor-info.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Speciality.name, schema: SpecialitySchema }]),
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([{ name: DoctorInfo.name, schema: DoctorInfoSchema }])
   ],
   controllers: [SpectialityController],
   providers: [SpectialityService]
